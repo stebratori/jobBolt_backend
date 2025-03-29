4e9267c835bdada1b14f2cb8786ed6e52a5650a8
Stefan Brankovic <stefanbrankovic@yahoo.com>
2025-03-29 16:05:57 +0100

    ChatGPT o1 model

--- services/chatGptService.js
+++ services/chatGptService.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export default class ChatGptService {
   constructor() {
-    this.model = 'gpt-4-turbo';
+    this.model = 'gpt-4-o1';
     
     // Configure axios instance with base URL and headers
     this.api = axios.create({
